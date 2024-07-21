f8794368b35de90c5f718f370572ef53c0c243a0
fredcui <yizhi.tsui@gmail.com>
2024-07-20 23:04:34 -0700

    compatibility

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,15 +4,13 @@ from .views import PostList, post_detail
 
 urlpatterns = [
     re_path(
-        r'^$', 
-        PostList.as_view(), 
-        { 'parent_template', 'base.html' },
+        r'^$',
+        PostList.as_view(),
         name='blog_post_list'),
     re_path(
         r'^(?P<year>\d{4})/'
         r'(?P<month>\d{1,2})/'
         r'(?P<slug>\[\w\-]+)/$',
         post_detail,
-        { 'parent_template': 'base.html' },
         name='blog_post_detail'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -12,20 +12,22 @@ from blogs.models import Post
 class PostList(View):
     def get(self, request, parent_template=None):
         return render(
-                request,
-                'blogs/post_list.html',
-                { 
-                 'post_list': Post._default_manager.all(), 
-                 'parent_template': parent_template
-                }
-            )
+            request,
+            'blogs/post_list.html',
+            {
+                'post_list': Post._default_manager.all(),
+                'parent_template': parent_template
+            }
+        )
+
 
 def post_detail(request, month, year, slug, parent_template=None):
     try:
-        got_post = Post._default_manager.filter(pub__date__year=year).filter(pub__date__month=month).get(slug__iexact=slug)
+        got_post = Post._default_manager.filter(pub__date__year=year).filter(
+            pub__date__month=month).get(slug__iexact=slug)
         return render(
-                request,
-                'blogs/post_detail.html',
-                { 'post': got_post, 'parent_template': parent_template })
+            request,
+            'blogs/post_detail.html',
+            {'post': got_post, 'parent_template': parent_template})
     except ObjectDoesNotExist:
         raise Http404
