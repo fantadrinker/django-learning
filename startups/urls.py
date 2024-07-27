3f61b1e6a2a7580b9037855e2bfaf97e17e5c4be
fandadrinker <fred.cui@macbookpro2.local>
2024-07-27 09:05:23 -0700

    nav

--- startups/urls.py
+++ startups/urls.py
@@ -3,8 +3,8 @@ from django.urls import re_path
 from .views import startup_detail, tag_list, tag_detail, startups_list
 
 urlpatterns = [
-    re_path(r'^tag/$', tag_list),
-    re_path(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail),
-    re_path(r'^startup/$', startups_list),
+    re_path(r'^tag/$', tag_list, name='startup_tags_list'),
+    re_path(r'^tag/(?P<slug>[\w\-]+)/$', tag_detail, name='startup_tag_detail'),
+    re_path(r'^startup/$', startups_list, name='startups_list'),
     re_path(r'^startup/(?P<slug>[\w\-]+)/$', startup_detail),
 ]
